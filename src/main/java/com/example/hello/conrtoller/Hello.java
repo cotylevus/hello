654d0f0b5bd358bcead8339d53892779ec1dafd5
BestIkun <mrteliu1107@gmail.com>
2023-07-04 16:29:39 +0800

    6

--- src/main/java/com/example/hello/conrtoller/Hello.java
+++ src/main/java/com/example/hello/conrtoller/Hello.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class Hello {
     @GetMapping("/hello")
     public String hello() {
+        System.out.println("发起了一次请求");
     return "hello";
     }
 }
